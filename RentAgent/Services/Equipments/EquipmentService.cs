159af9c82a6e780ac87af8fc879a976ea4be634a
TonyAnthony1 <tonyanthonyyyyy@gmail.com>
2026-03-27 17:24:22 +0100

    modyfikacja interfejsu i klasy do obsługi sprzętu

--- RentAgent/Services/Equipments/EquipmentService.cs
+++ RentAgent/Services/Equipments/EquipmentService.cs
@@ -1,6 +1,41 @@
-﻿namespace RentAgent.Services.Equipments;
+﻿using RentAgent.Enums;
+using RentAgent.Models;
 
-public class EquipmentService
+namespace RentAgent.Services.Equipments;
+
+public class EquipmentService : IEquipmentService
 {
+    private List<Equipment> _equipments = new();
+
+    public void AddEquipment(Equipment equipment)
+    {
+        _equipments.Add(equipment);
+    }
+
+    public List<Equipment> GetEquipments()
+    {
+        return _equipments.ToList();
+    }
+
+    public List<Equipment> GetAvailableEquipments()
+    {
+        return _equipments.Where(e => e.IsAvailable()).ToList();
+    }
+
+    public Equipment GetEquipmentById(int id)
+    {
+        return _equipments.FirstOrDefault(e => e.Id == id);
+    }
+
+    public void MarkEquipmentAsUnavailable(Equipment equipment)
+    {
+        var found = GetEquipmentById(equipment.Id);
+        if (found == null)
+            Console.WriteLine("Equipment not found");
+        if (found.Status == EquipmentStatus.Reserved)
+            Console.WriteLine("Equipment is reserved at the moment");
+
+        found.Status = EquipmentStatus.Unavailable;
+    }
     
 }
--- RentAgent/Services/Equipments/IEquipmentService.cs
+++ RentAgent/Services/Equipments/IEquipmentService.cs
@@ -1,6 +1,15 @@
-﻿namespace RentAgent.Services.Equipments;
+﻿using RentAgent.Models;
+
+namespace RentAgent.Services.Equipments;
 
 public interface IEquipmentService
 {
+    public List<Equipment> GetEquipments();
+    public List<Equipment> GetAvailableEquipments();
+    Equipment GetEquipmentById(int id);
+    public void AddEquipment(Equipment equipment);
+    public void MarkEquipmentAsUnavailable(Equipment equipment);
+    
+    
     
 }
