7ecdf3dba1b97a500a58be623e085ee2f9003172
TonyAnthony1 <tonyanthonyyyyy@gmail.com>
2026-03-27 18:35:50 +0100

    modyfikacja pakietu UserService

--- RentAgent/Services/Users/IUserService.cs
+++ RentAgent/Services/Users/IUserService.cs
@@ -1,6 +1,10 @@
-﻿namespace RentAgent.Services.Users;
+﻿using RentAgent.Models;
+
+namespace RentAgent.Services.Users;
 
 public interface IUserService
 {
-    
+    List<User> GetAllUsers();
+    User? GetById(int id);
+    void AddUser(User user);
 }
--- RentAgent/Services/Users/UserService.cs
+++ RentAgent/Services/Users/UserService.cs
@@ -1,6 +1,21 @@
-﻿namespace RentAgent.Services.Users;
+﻿using RentAgent.Models;
 
-public class UserService
+namespace RentAgent.Services.Users;
+
+public class UserService:IUserService
 {
+    private  List<User> _users = new();
     
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+    public List<User> GetAllUsers()
+    {
+        return _users.ToList();
+    }
+    public User? GetById(int id)
+    {
+        return _users.FirstOrDefault(u => u.Id == id);
+    }
 }
